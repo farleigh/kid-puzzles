0ed7ef71931c2f7d5a95cf540cddb2ab2243644b
Clinton Farleigh <farleigh@gmail.com>
2021-02-07 11:38:11 -0800

    Added java puzzles

---
+++ java/src/main/ca/farleigh/kidpuzzles/StringReverser.java
@@ -0,0 +1,9 @@
+package ca.farleigh.kidpuzzles;
+
+public class StringReverser {
+
+    public String reverse(final String str) {
+        // TODO: Put your solution here
+        throw new UnsupportedOperationException("Add your solution!");
+    }
+}
---
+++ java/src/test/StringReverserTest.java
@@ -0,0 +1,34 @@
+import ca.farleigh.kidpuzzles.StringReverser;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringReverserTest {
+
+    private StringReverser reverser;
+
+    @Before
+    public void setup() {
+        this.reverser = new StringReverser();
+    }
+
+    @Test
+    public void testReverseEmptyString() {
+        Assert.assertEquals("", reverser.reverse(""));
+    }
+
+    @Test
+    public void testReverseSmallString() {
+        Assert.assertEquals("cba", reverser.reverse("abc"));
+    }
+
+    @Test
+    public void testReverseWord() {
+        Assert.assertEquals("Chicken", reverser.reverse("Nekcihc"));
+    }
+
+    @Test
+    public void testReverseSentence() {
+        Assert.assertEquals("Emag oediv a si tfarcenim.", reverser.reverse("Minecraft is a video game."));
+    }
+}
